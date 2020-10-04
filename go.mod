6a6c2bf2216685e71e8023b809ab1a97b01e2e01
gaischen <gaischen@163.com>
2020-10-04 22:12:11 +0800

    access to colly

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/skyline/skyline-spider
 
 go 1.14
+
+require github.com/gocolly/colly/v2 v2.1.0
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
 import "fmt"
+import colly "github.com/gocolly/colly/v2"
 
-func main()  {
+func main() {
 	fmt.Println("test")
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"), )
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://hackerspaces.org/")
 }
