f109e9052c442cfd7c48ddc38c0604951867af97
gaischen <gaischen@163.com>
2020-10-07 22:42:41 +0800

    commit for poster

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/skyline/skyline-spider/agent"
+	"github.com/skyline/skyline-spider/poster"
+)
 import colly "github.com/gocolly/colly/v2"
 
 func main() {
 	fmt.Println("test")
 	c := colly.NewCollector(
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"), )
+		colly.AllowedDomains("www.shein.com"),
+		colly.UserAgent(agent.GetChromeUserAgent()),
+		colly.AllowURLRevisit(),
+	)
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+	c.OnHTML("span", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -23,6 +24,17 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Start scraping
+	//err := c.Visit("https://www.shein.com/BLUES-High-Waisted-Jeggings-Without-Pocket-p-1587411-cat-1934.html?scici=navbar_WomenHomePage~~tab01navbar05~~5~~webLink~~SPcCccWomenCategory~~0~~50001")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+
+	p := poster.NewHttpPoster()
+
+	p.OnHtml("href", func(content string) {
+		fmt.Println(content)
+	})
+	p.Call("http://www.baidu.com", nil)
+
 }
---
+++ poster/poster.go
@@ -0,0 +1,53 @@
+package poster
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+//http header
+type PosterHead struct {
+	header map[string]string
+}
+
+type HtmlCallback func(content string)
+
+type Poster interface {
+	Call(url string, head *PosterHead)
+}
+
+type HttpPoster struct {
+	callbacks   []HtmlCallback
+	queryString string
+	lock *sync.RWMutex
+}
+
+
+func NewHttpPoster() *HttpPoster {
+	return &HttpPoster{callbacks: make([]HtmlCallback, 0)}
+}
+
+func (h *HttpPoster) Call(url string, head *PosterHead) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Errorf("error", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Errorf("read body error", err)
+	}
+
+	if len(h.callbacks) > 0 {
+		for _, cc := range h.callbacks {
+			cc(string(body))
+		}
+	}
+}
+
+func (h *HttpPoster) OnHtml(query string, callback HtmlCallback) {
+	h.queryString = query
+	h.callbacks = append(h.callbacks, callback)
+}
