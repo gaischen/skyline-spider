eb6a93d6e7066be2f34d21b5b6b3cfd75e977a0a
gaischen <gaischen@163.com>
2020-10-28 17:21:26 +0800

    test

--- poster/fib_test.go
+++ poster/fib_test.go
@@ -1,9 +1,11 @@
 package poster
 
 import (
+	"context"
 	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestFibList(t *testing.T) {
@@ -39,4 +41,47 @@ func TestArray(t *testing.T) {
 
 func change(i int) {
 	i = i + 1
+}
+
+func TestSelect(t *testing.T) {
+	done := make(chan struct{})
+	go ticker(done)
+	time.Sleep(3 * time.Second)
+	done <- struct{}{}
+	close(done)
+	time.Sleep(3 * time.Second)
+}
+
+func ticker(done chan struct{}) {
+	t := time.NewTicker(1 * time.Second)
+	for range t.C {
+		select {
+		case <-done:
+			fmt.Println("done exit")
+			return
+		default:
+			fmt.Println("tick")
+		}
+	}
+}
+
+func TestContext(t *testing.T) {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go tickerContext(ctx)
+	time.Sleep(3 * time.Second)
+	cancelFunc()
+	time.Sleep(3 * time.Second)
+}
+
+func tickerContext(ctx context.Context) {
+	t := time.NewTicker(1 * time.Second)
+	for range t.C {
+		select {
+		case <-ctx.Done():
+			fmt.Println("done...")
+			return
+		default:
+			fmt.Println("tick...")
+		}
+	}
 }
