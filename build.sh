adc6e8f4c84eed52b9a206fb15042a801df934c1
vanga <gaischen@163.com>
2020-11-02 10:27:18 +0800

    build

--- build.sh
+++ build.sh
@@ -14,14 +14,13 @@ go build main.go
 echo "build main.go successful"
 
 ##进程历史
-pid = $(ps -ef | grep "main" | grep -v grep | awk '{print $2}')
-echo "found go pid:$pid"  
+pid=$(ps -ef | grep "main" | grep -v grep | awk '{print $2}')
+echo "found go pid:$pid"
 
 if ps -p $pid > /dev/null
-
-then
-   echo "$pid is running"
-   echo "killing $pid"
+  then
+     echo "$pid is running"
+     echo "killing $pid"
 fi
 
 
